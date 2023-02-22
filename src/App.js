c9bd55c6f7a8d416de7c5486886e94abdda2b66d
Hazem Abouassi <hazem.abouassi@dci.education>
2023-02-22 08:26:25 +0100

    add react countries app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,21 @@ import ByRegion from "./components/views/ByRegion";
 import ByName from "./components/views/ByName";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
+import { Route, Routes } from "react-router-dom";
+import OneCountry from "./components/views/OneCountry";
 
 function App() {
-
   return (
     <div className="App">
-{/* show the Header component, then add the routes to the pages components*/}
-
-   App component
+      <Header />
+      {/* show the Header component, then add the routes to the pages components*/}
+      <Routes>
+        <Route path="/" element={<AllCountries />} />
+        <Route path="/by-region" element={<ByRegion />} />
+        <Route path="/by-name" element={<ByName />} />
+        <Route path="/country/:code" element={<OneCountry />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
     </div>
   );
 }
--- src/components/views/ByRegion.js
+++ src/components/views/ByRegion.js
@@ -1,29 +1,66 @@
 import React, { useEffect, useState } from "react";
 import CountryCard from "../CountryCard";
+ //save the user input in the pre-made local state (region), then fetch all countries of that region on page load and make sure it gets executed every time the state (region) changes (what hook should you use for that? and what should you add to it?) then save the received array of countries in a local state (call it countries)
 
 export default function ByRegion() {
-  //save the user input in the pre-made local state (region), then fetch all countries of that region on page load and make sure it gets executed every time the state (region) changes (what hook should you use for that? and what should you add to it?) then save the received array of countries in a local state (call it countries)
-
+ 
+  const [countries, setCountries] = useState([]);
   const [region, setRegion] = useState("Africa");
-  
 
-  const handleChange = () => {};
+  useEffect(() => {
+    fetch(`https://restcountries.com/v2/region/${region}`)
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  }, [region]);
 
+  const handleChange = (e) => {
+    setRegion(e.target.value)
+ 
+  };
 
   return (
     <div>
-      <input type={"radio"} name="region" defaultChecked value="Africa" />
+      <input
+        type={"radio"}
+        name="region"
+        defaultChecked
+        value="Africa"
+        onChange={handleChange}
+      />
       <label style={{ marginRight: "15px" }}>Africa</label>
-      <input type={"radio"} name="region" value="America" />
+      <input
+        type={"radio"}
+        name="region"
+        value="Americas"
+        onChange={handleChange}
+      />
       <label style={{ marginRight: "15px" }}>America</label>
-      <input type={"radio"} name="region" value="Asia" />
+      <input
+        type={"radio"}
+        name="region"
+        value="Asia"
+        onChange={handleChange}
+      />
       <label style={{ marginRight: "15px" }}>Asia</label>
-      <input type={"radio"} name="region" value="Europe" />
+      <input
+        type={"radio"}
+        name="region"
+        value="Europe"
+        onChange={handleChange}
+      />
       <label style={{ marginRight: "15px" }}>Europe</label>
-      <input type={"radio"} name="region" value="Oceania" />
+      <input
+        type={"radio"}
+        name="region"
+        value="Oceania"
+        onChange={handleChange}
+      />
       <label>Oceania</label>
 
       <div className="cardsContainer">
+        {countries.map((item, i) => (
+          <CountryCard key={i} country={item} />
+        ))}
         {/* show the countries you received from fetch and saved in the local state (use CountryCard component to display each individual one) */}
       </div>
     </div>
